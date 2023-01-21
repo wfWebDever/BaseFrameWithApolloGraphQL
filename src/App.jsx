7b2cc3e8cc2cd4836583a2d4fea58b69e7984a61
wfWebDever <xinyilan2005@gmail.com>
2023-01-21 16:42:17 +0800

    Keep calm and commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import React from 'react'
 import { useRoutes } from 'react-router-dom'
-import routes from './routes/routers';
+import routes from './routes/routers'
 import './App.css'
 
-const App = () => (
+const App = () => {
   // TODO
+  const a = 1
 
-  <div className="App">{useRoutes(routes)}</div>
-)
+  return <div className="App">{useRoutes(routes)}</div>
+}
 
 export default App
