4ecd7808704e2817049e48bd0a13ca7752ab624e
wfWebDever <xinyilan2005@gmail.com>
2023-01-26 21:10:45 +0800

    chore: auth route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-// import React from 'react'
+import { useState, useEffect } from 'react'
 import { useRoutes } from 'react-router-dom'
-import routes from './routes/routers'
+import { routes, privateRoutes } from './routes'
 import './App.css'
 
 const App = () => {
-  // TODO
-  const a = 1
+  // get the auth for the project
+  const adminAuth = useRoutes(routes)
+  const unLoginAuth = useRoutes(privateRoutes)
+  const [loading, setLoading] = useState(true)
+  const auth = 1
 
-  return <div className="App">{useRoutes(routes)}</div>
+  useEffect(() => {
+    setLoading(false)
+    // TODO login
+    // const userId = localStorage.getItem('userId')
+    // if (userId) {
+    //   setLoading(false)
+    // }
+  }, [])
+
+  if (loading) {
+    return <div>loading......</div>
+  }
+
+  return <div className="App">{auth ? adminAuth : unLoginAuth}</div>
 }
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  ApolloLink,
+  from
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
+
 import './index.css'
 import App from './App'
 
+// Log any GraphQL errors or network error that occurred
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    )
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+  if (networkError.statusCode === 401)
+    console.log(`[Network 401]: ${networkError}`)
+})
+
+const httpLink = new HttpLink({
+  uri: 'https://api.spacex.land/graphql' // maybe use .env
+})
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  const token = localStorage.getItem('token')
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }))
+
+  return forward(operation)
+})
+
 // Apollo config
 const client = new ApolloClient({
-  uri: 'https://api.spacex.land/graphql',
+  link: from([authMiddleware, errorLink, httpLink]), // multiple custom links
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
@@ -32,13 +70,16 @@ const client = new ApolloClient({
             // the existing list items.
             // eslint-disable-next-line default-param-last
             merge(existing = [], incoming, { args: { offset = 0 } }) {
+              console.info(offset)
               return [...existing, ...incoming]
             }
           }
         }
       }
     }
-  })
+  }),
+  // credentials: 'include',
+  connectToDevTools: true
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
